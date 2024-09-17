107a26671877928619dcfdac7e69f9305b1d895f
Nhunk <anhnhung.edu@gmail.com>
2024-09-18 02:28:57 +0700

    Days 4+

---
+++ stack.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+struct node {
+    int data;
+    node *next;
+};
+struct myStack{
+    node *L;
+};
+void create(myStack &S);//Khởi tạo danh sách liên kết đơn
+int empty(myStack S);//Check danh sách liên kết đơn có đang rỗng hay không
+void push(myStack &S, int x);//Đưa vào
+void top(myStack S, int &x); // Lấy mà không xóa
+void pop(myStack &S);//Lấy và xóa
+
+int main(){
+    myStack S; int x;
+    create(S);
+    push(S, 1);
+    push(S, 2);
+    push(S,3);
+    top(S, x);
+    pop(S);
+    printf("\n x = %d", x);
+
+}
+void create(myStack &S){
+    S.L=NULL;
+}
+int empty(myStack S){
+    return S.L==NULL;
+}
+void push(myStack &S, int x){
+    node *p = new node;
+    p->data = x;
+    p->next = NULL;
+    if(empty(S)) S.L = p;
+    else {
+        node *q = S.L;
+        while(q->next!=NULL){
+            q = q->next;
+        }
+        p = q-> next;
+    }
+}
+void top(myStack S, int &x){
+    if(empty(S)){
+        node *q =S.L;
+        while(q->next !=NULL){
+            q=q->next;
+        } 
+        x=q->data;
+
+    }
+}
+void pop(myStack &S){
+    node *p;
+    if(!empty){
+        if(S.L->next ==NULL){
+            p = S.L;
+            S.L = NULL;
+        }
+        else{
+        node *q = S.L;
+        while(q->next->next !=NULL){
+            q=q->next;
+        }
+        p=q->next;
+        p->next=NULL;
+        }
+        delete(p);
+    }
+}
